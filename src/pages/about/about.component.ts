e10f9a45590a9c5dc230c8a37f5a4d3f0f0ba719
jinalmehta27 <jinalmehta.jam@gmail.com>
2024-09-14 14:57:16 +0530

    first commit

---
+++ src/pages/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  template: ` This is About Us page...`,
+  styles: `
+  
+  `,
+})
+export class AboutComponent {}
